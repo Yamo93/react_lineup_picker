24f400cfdf77b10c20727ac5cef4b15af6f9d46e
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2018-11-02 17:12:25 +0300

    added grids for responsiveness

--- src/components/SelectionChooser/SelectionChooser.js
+++ src/components/SelectionChooser/SelectionChooser.js
@@ -5,25 +5,22 @@ const selectionChooser = (props) => {
     //console.log(props.players);
     return (
         <div className={classes.Selection}>
-        <ul className={classes.playerNameList}>
+        <div className={classes.playerWrapper}>
             <p className={classes.playerLabel}>Player: </p>
+            <p className={classes.numberLabel}>Number: </p>
+            <p className={classes.playerLabelAdded}>Player: </p>
+            <p className={classes.numberLabelAdded}>Number: </p>
             {/* enter these dynamically */}
             {props.players.map(player => {
                 return (
-                    <li key={player.id}><input type="text" onChange={(event) => props.changedName(event, player.id)} placeholder={player.playerName} value={player.playerName} className={classes.inputName} /></li>
+                    <>
+                    <input key={player.id} type="text" onChange={(event) => props.changedName(event, player.id)} placeholder={player.playerName} value={player.playerName} className={classes.inputName} />
+                    <input key={player.id} type="number" onChange={(event) => props.changedNum(event, player.id)} min="1" max="40" value={player.playerNumber} placeholder={player.playerNumber} className={classes.inputNum} />
+                    </>
                 );
             })}
             
-        </ul>
-        <ul className={classes.playerNumberList}>
-            <p className={classes.numberLabel}>Number: </p>
-            {/* enter these dynamically */}
-            {props.players.map(player => {
-                return (
-                    <li key={player.id}><input type="number" onChange={(event) => props.changedNum(event, player.id)} min="1" max="40" value={player.playerNumber} placeholder={player.playerNumber} className={classes.inputNum} /></li>
-                );
-            })}
-        </ul>
+        </div>
     </div>
     );
 };
