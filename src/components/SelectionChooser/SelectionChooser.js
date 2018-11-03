464a8636f38544620523ad6b97741cb25dd7635e
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2018-11-03 11:40:25 +0300

    changed some components to stateful, fixed bug

--- src/components/SelectionChooser/SelectionChooser.js
+++ src/components/SelectionChooser/SelectionChooser.js
@@ -1,28 +1,33 @@
-import React, {Fragment} from 'react';
+import React, { Component } from 'react';
 import classes from './SelectionChooser.module.css';
+import PlayerInput from '../../containers/PlayerInput/PlayerInput';
 
-const selectionChooser = (props) => {
-    //console.log(props.players);
-    return (
-        <div className={classes.Selection}>
-        <div className={classes.playerWrapper}>
-            <p className={classes.playerLabel}>Player: </p>
-            <p className={classes.numberLabel}>Number: </p>
-            <p className={classes.playerLabelAdded}>Player: </p>
-            <p className={classes.numberLabelAdded}>Number: </p>
-            {/* enter these dynamically */}
-            {props.players.map(player => {
-                return (
-                    <Fragment key={player.playerNumber}>
-                    <input key={player.playerName} type="text" onChange={(event) => props.changedName(event, player.id)} placeholder={player.playerName} value={player.playerName} className={classes.inputName} />
-                    <input key={player.id} type="number" onChange={(event) => props.changedNum(event, player.id)} min="1" max="40" value={player.playerNumber} placeholder={player.playerNumber} className={classes.inputNum} />
-                    </Fragment>
-                );
-            })}
-            
+class SelectionChooser extends Component {
+    /*componentDidMount() {
+        console.log('this got run');
+        this.inputElement.focus();
+    }*/
+
+    render () {
+        return (
+            <div className={classes.Selection}>
+            <div className={classes.playerWrapper}>
+                <p className={classes.playerLabel}>Player: </p>
+                <p className={classes.numberLabel}>Number: </p>
+                <p className={classes.playerLabelAdded}>Player: </p>
+                <p className={classes.numberLabelAdded}>Number: </p>
+                {/* enter these dynamically */}
+                {this.props.players.map((player, index) => {
+                    return (
+                        <PlayerInput position={index} key={index} nameKey={player.playerNumber} numKey={player.id} changedName={this.props.changedName} changedNum={this.props.changedNum} namePlaceholder={player.playerName} numPlaceholder={player.playerNumber} nameValue={player.playerName} numValue={player.playerNumber} />
+                    );
+                })}
+                
+            </div>
         </div>
-    </div>
-    );
-};
+        );
+    }
+}
 
-export default selectionChooser;
+
+export default SelectionChooser;
---
+++ src/containers/PlayerInput/PlayerInput.js
@@ -0,0 +1,17 @@
+import React, { Fragment, Component } from 'react';
+import classes from '../../components/SelectionChooser/SelectionChooser.module.css';
+
+class PlayerInput extends Component {
+    render() {
+        return (
+            <Fragment key={this.props.numValue + 1}>
+            <input ref={(input) => {this.inputElement = input }} type="text" onChange={(event) => this.props.changedName(event, this.props.numKey)} placeholder={this.props.namePlaceholder} value={this.props.nameValue} className={classes.inputName} />
+            <input type="number" onChange={(event) => this.props.changedNum(event, this.props.numKey)} min="1" max="40" value={this.props.numValue} placeholder={this.props.numPlaceholder} className={classes.inputNum} />
+            {/* you have to outsource this into a separate playerInput component. then, pass the index from here down to that input component, and let that inputcomponent be a class. then run the componentDidMount() on that component and let it focus... hopefully it'll work :/ */}
+            </Fragment>
+
+        );
+    }
+}
+
+export default PlayerInput;
